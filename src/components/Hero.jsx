525a5b0cb66a1684a6a870884a1a003a4ddb89b7
DOTfei <khaijin.yap@gmail.com>
2025-12-16 23:12:43 +0800

    Update Hero section design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,6 +20,17 @@ const Hero = () => {
                     </p>
 
                 </div>
+                <div className="hero-cta-group">
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfaGcplKHxYEXqINu1teICF6C69kC0LGCQpUFGiLBwQXHlIPA/viewform?usp=dialog" className="btn btn-primary hero-cta" target="_blank" rel="noopener noreferrer">
+                        Book Now (Cambridge)
+                    </a>
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfR387buFFREjO0ueyNFqfpVL_NSkwKw29B61eJd1T6FPjKgw/viewform?usp=dialog" className="btn btn-primary hero-cta" target="_blank" rel="noopener noreferrer">
+                        Book Now (MUET)
+                    </a>
+                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSdM3I8R7U66GPOKsLFc2aBy1j38V5Hh9Bc5fR9gzDvctPmN-w/viewform?usp=dialog" className="btn btn-primary hero-cta" target="_blank" rel="noopener noreferrer">
+                        Book Now (Speaking Class)
+                    </a>
+                </div>
                 {/* Optional: Add a hero image here if available, or use a background image */}
             </div>
         </section>
